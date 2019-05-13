67f4ad8cdc191647b3bdf86587f5093662f173c9
Jordi Berns <jordi_berns@live.nl>
2019-05-13 15:40:46 +0200

    piece class/repo/service/controller

---
+++ src/main/java/nl/yer/middleman/dreampoint/api/PieceController.java
@@ -0,0 +1,62 @@
+package nl.yer.middleman.dreampoint.api;
+
+import nl.yer.middleman.dreampoint.model.Piece;
+import nl.yer.middleman.dreampoint.service.PieceService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("api/pieces")
+@CrossOrigin("*")
+
+public class PieceController {
+
+    private PieceService pieceService;
+
+    public PieceController(PieceService pieceService) {this.pieceService = pieceService;}
+
+    @GetMapping
+    public Iterable<Piece> list() {return pieceService.findAll();}
+
+    @GetMapping("{id}")
+    public Piece get(@PathVariable long id) {
+        Optional<Piece> optionalPiece = this.pieceService.findById(id);
+        if(optionalPiece.isPresent()) {
+            Piece result = optionalPiece.get();
+            return result;
+        }
+        else {
+            return null;
+        }
+    }
+
+    @PostMapping
+    public Piece create(@RequestBody Piece piece) {return this.pieceService.save(piece);}
+
+    @PutMapping("{id}")
+    public Piece update(@PathVariable long id, @RequestBody Piece input) {
+        Optional<Piece> optionalPiece = this.pieceService.findById(id);
+        if(optionalPiece.isPresent()) {
+            Piece target = optionalPiece.get();
+
+            target.setName(input.getName());
+            target.setyPosition(input.getyPosition());
+            target.setxPosition(input.getxPosition());
+
+            return this.pieceService.save(target);
+        }
+        else {
+            return null;
+        }
+    }
+
+    @DeleteMapping("{id}")
+    public void delete(@PathVariable long id) {
+        Optional<Piece> optionalPiece = this.pieceService.findById(id);
+        if(optionalPiece.isPresent()) {
+            this.pieceService.deleteById(id);
+        }
+    }
+
+}
---
+++ src/main/java/nl/yer/middleman/dreampoint/model/Piece.java
@@ -0,0 +1,46 @@
+package nl.yer.middleman.dreampoint.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Piece {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private int yPosition;
+    private int xPosition;
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getyPosition() {
+        return yPosition;
+    }
+
+    public void setyPosition(int yPosition) {
+        this.yPosition = yPosition;
+    }
+
+    public int getxPosition() {
+        return xPosition;
+    }
+
+    public void setxPosition(int xPosition) {
+        this.xPosition = xPosition;
+    }
+
+}
