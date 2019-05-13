29b99415bf6e78cb12594174fdbbbc5e76b0ecff
tomasheldeweg <tomasheldeweg@gmail.com>
2019-05-13 15:18:57 +0200

    Add player class/controller/repo/service

--- src/main/java/nl/yer/middleman/dreampoint/api/PlayerController.java
+++ src/main/java/nl/yer/middleman/dreampoint/api/PlayerController.java
@@ -1,8 +1,38 @@
 package nl.yer.middleman.dreampoint.api;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import nl.yer.middleman.dreampoint.model.player.Player;
+import nl.yer.middleman.dreampoint.service.PlayerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
+@RequestMapping("api/players")
 public class PlayerController {
+
+    @Autowired
+    private PlayerService playerService;
+
+    @GetMapping
+    public Iterable<Player> list(){
+        return this.playerService.findAll();
+    }
+
+    @GetMapping("{id}")
+    public Player get(@PathVariable long id){
+        Optional<Player> optionalResult = this.playerService.findById(id);
+        if (optionalResult.isPresent()) {
+            Player result = optionalResult.get();
+            return result;
+        }
+        else {
+            return null; // moet veranderen
+        }
+    }
+
+    @PostMapping
+    public Player create(@RequestBody Player player){
+        return this.playerService.save(player);
+    }
 }
--- src/main/java/nl/yer/middleman/dreampoint/service/PlayerService.java
+++ src/main/java/nl/yer/middleman/dreampoint/service/PlayerService.java
@@ -1,8 +1,11 @@
 package nl.yer.middleman.dreampoint.service;
 
+import nl.yer.middleman.dreampoint.model.player.Player;
 import nl.yer.middleman.dreampoint.persistence.PlayerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class PlayerService {
@@ -10,5 +13,15 @@ public class PlayerService {
     @Autowired
     private PlayerRepository playerRepository;
 
-    
+    public Player save(Player player){
+        return this.playerRepository.save(player);
+    }
+
+    public Iterable<Player> findAll(){
+        return this.playerRepository.findAll();
+    }
+
+    public Optional<Player> findById(long id){
+        return this.playerRepository.findById(id);
+    }
 }
