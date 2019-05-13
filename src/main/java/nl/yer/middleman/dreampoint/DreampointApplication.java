6d32d1ddb5585175e0de7dd64517f56803d42174
Jordi Berns <jordi_berns@live.nl>
2019-05-13 14:14:23 +0200

    first commit

---
+++ src/main/java/nl/yer/middleman/dreampoint/DreampointApplication.java
@@ -0,0 +1,13 @@
+package nl.yer.middleman.dreampoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreampointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DreampointApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/yer/middleman/dreampoint/DreampointApplicationTests.java
@@ -0,0 +1,16 @@
+package nl.yer.middleman.dreampoint;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DreampointApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
